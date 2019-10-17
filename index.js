22d666683e3d0631afb6a9d90649da94c80c78ce
dgrusemann <dominik.grusemann@gmail.com>
2019-10-17 12:15:09 +0200

    [nostory] initial rules

---
+++ index.js
@@ -0,0 +1,19 @@
+module.exports = {
+    globals: {
+        chai: true,
+        chaiHttp: true,
+        expect: true,
+        factory: true,
+        nock: true,
+        sinon: true
+    },
+    rules: {
+        "mocha/no-exclusive-tests": "error",
+        "flowtype/no-types-missing-file-annotation": 0,
+        "no-var": "error",
+        "strict": "error",
+        "no-console": "error",
+        "require-await": "error",
+        "import/no-default-export": "error"
+    }
+};

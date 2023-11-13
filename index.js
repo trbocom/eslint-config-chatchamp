38781b37ced9ff425398978f294e89f04e8b2497
dgrusemann <dominik.grusemann@gmail.com>
2023-11-13 09:47:52 +0100

    chore: remove flow from standard config [sc-19253]

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ module.exports = {
     extends: [
         "eslint:recommended",
         "standard",
-        "plugin:flowtype/recommended",
         "plugin:promise/recommended"
     ],
     globals: {
@@ -15,25 +14,19 @@ module.exports = {
     },
     plugins: [
         "babel",
-        "flowtype",
         "mocha",
         "import"
     ],
     rules: {
         "mocha/no-exclusive-tests": "error",
-        "flowtype/no-types-missing-file-annotation": 0,
         "no-var": "error",
         "strict": "error",
         "no-console": "error",
         "require-await": "error",
         "import/no-default-export": "error",
         "import/newline-after-import": "error",
-        "lines-between-class-members": ["error", "always", { exceptAfterSingleLine: true }],
+        "lines-between-class-members": ["error", "always", {exceptAfterSingleLine: true}],
         "max-classes-per-file": ["error", 1]
     },
-    settings: {
-        flowtype: {
-            onlyFilesWithFlowAnnotation: true
-        }
-    }
+    settings: {}
 };
